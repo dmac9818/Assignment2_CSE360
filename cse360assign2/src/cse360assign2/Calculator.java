20072f140eba125148a567c785c97b705bb20c69
dmac9818 <47962297+dmac9818@users.noreply.github.com>
2019-02-25 04:58:51 -0700

    Creation of calculator class

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,38 @@
+package cse360assign2;
+/** Name: Daniel McComb
+ *Class ID: 485
+ *Assignment 2
+ *Description: A class that contains methods to perform a number of math operations and print the history of them.
+ */
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign2/src/cse360assign2/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Demineon
+ *
+ */
+package cse360assign2;
