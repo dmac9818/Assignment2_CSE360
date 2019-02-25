8793f1a65fe08ef9b41aad844df567913b671d26
dmac9818 <47962297+dmac9818@users.noreply.github.com>
2019-02-25 05:07:13 -0700

    Built getHistory function and added changes to previous constructor,
    add, subtract, multiply and divide functions to add correct data to
    history string.

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -7,9 +7,10 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
-	
+	private String history;
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	
@@ -22,45 +23,54 @@ public class Calculator {
 	}
 	
 	/**
-	 * Adds parameter to total value
+	 * Adds parameter to total value. Updates history string to show add operation
 	 * @param value integer taken to be added to total
 	 */
 	
 	public void add (int value) {
 		total = total + value;
+		history=history+" + "+ value;
 	}
 	
 	/**
-	 * Subtracts parameter from total value
+	 * Subtracts parameter from total value. Updates history string to show subtract operation
 	 * @param value integer taken to be subtracted from total
 	 */
 	
 	public void subtract (int value) {
 		total = total - value;
+		history=history+" - "+ value;
 	}
 	
 	/**
-	 * Multiplies parameter with total value
+	 * Multiplies parameter with total value. Updates history string to show multiply operation
 	 * @param value integer taken to be multiplied with total
 	 */
 	
 	public void multiply (int value) {
 		total = total * value;
+		history=history+" * "+ value;
 	}
 	
 	/**
-	 * Divides total value by parameter
+	 * Divides total value by parameter. Updates history string to show divide operation
 	 *  @param value integer taken to be divisor of total
 	 */
 	
 	public void divide (int value) {
-		if(value==0)
+		if(value==0) //Avoids diving by 0
 			total=0;
-		else
+		else 
 			total = total/value;
+		
+		history=history+" / "+ value;
 	}
 	
+	/**
+	 * Returns the history string
+	 */
+	
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
